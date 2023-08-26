8130a6c2b9cf7c8b08d46acc2b1a6d629ec7b7e1
yasir-dev1 <112517455+yasir-dev1@users.noreply.github.com>
2023-08-26 13:28:10 +0300

    Change pdf libary because old libary have bugs.

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
+from fpdf import FPDF
 
 url = "https://www.w3resource.com/python-exercises/python-conditional-statements-and-loop-exercises.php"
 
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'html.parser')
+
+# PDF dosyası oluştur
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial", size=12)
+
 paragraphs = soup.find_all("p")
 pattern = r"\d+\.\s"
 
-# Collect the paragraphs that match the pattern
-matching_paragraphs = []
-
 for paragraph in paragraphs:
     strong = paragraph.find("strong")
-    
+
+    pattern = r"\d+\.\s"
+
     if strong is not None and re.match(pattern, strong.text):
-        matching_paragraphs.append(paragraph.get_text())
+        # Paragrafı PDF dosyasına yazdır
+        pdf.multi_cell(0, 10, paragraph.get_text())
+        pdf.ln()
 
-# Write matching paragraphs to a PDF file
-pdf_filename = "result.pdf"
+# PDF dosyasını kaydet
+pdf.output("paragraph_result.pdf")
 
-c = canvas.Canvas(pdf_filename, pagesize=letter)
-text = "\n\n".join(matching_paragraphs)
-
-# Add the text to the PDF
-c.drawString(100, 700, text)
-c.save()
-
-print(f"Matching paragraphs saved to {pdf_filename}")
+print("PDF dosyası oluşturuldu: paragraph_result.pdf")
